3a9a63f07e31a0074cb1ec9334c20be0f150047c
likhitha <sn596@njit.edu>
2018-10-05 19:31:25 -0400

    Task:Fetch,print data in array

--- public/index.php
+++ public/index.php
@@ -5,3 +5,21 @@
  * Date: 10/4/18
  * Time: 7:13 PM
  */
+main::start();
+
+class main
+{
+
+    static public function start()
+    {
+
+        $file = fopen("example.csv", "r");
+
+        while (!feof($file)) {
+            print_r(fgetcsv($file));
+        }
+
+        fclose($file);
+
+    }
+}
