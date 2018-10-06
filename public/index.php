d447c323334c3bd212643683badbc3a557beccf6
likhitha <sn596@njit.edu>
2018-10-06 13:28:00 -0400

    Feature:Function setup for records display

--- public/index.php
+++ public/index.php
@@ -5,22 +5,34 @@
  * Date: 10/4/18
  * Time: 7:13 PM
  */
-main::start( );
+main::start( "example.csv");
 
 class main{
 
-    static public function start(){
+    static public function start($filename){
 
-        $file = fopen("example.csv","r");
-            while(! feof($file))
-            {
-                $record = fgetcsv($file);
+        $records = csv::getRecords($filename);
+         print_r($records);
 
-                $records[]= $record;
-            }
+
+
+    }
+}
+
+class csv{
+
+    static public function getRecords($filename){
+        $file = fopen($filename,"r");
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[]= $record;
+        }
 
         fclose($file);
-            print_r($records);
+        return $records;
+
 
 
     }
