ba820c122fe0f49a74ef9ed2e2b2cdb97f4f0c12
likhitha <sn596@njit.edu>
2018-10-06 14:20:50 -0400

    Task:Labelling objects

--- public/index.php
+++ public/index.php
@@ -12,9 +12,8 @@ class main{
     static public function start($filename){
 
         $records = csv::getRecords($filename);
-        
 
-         print_r($records);
+
 
 
 
@@ -24,12 +23,14 @@ class main{
 class csv{
 
     static public function getRecords($filename){
+
         $file = fopen($filename,"r");
+
         while(! feof($file))
         {
             $record = fgetcsv($file);
 
-            $records[]= recordFactory::create();
+            $records[]= recordFactory::create($record);
         }
 
         fclose($file);
@@ -42,13 +43,21 @@ class csv{
 
 class record{
 
+    public function __construct(Array $record=null){
+
+        print_r($record);
+}
+
+
 }
 
 class recordFactory{
 
     public static function create(Array $array = null){
 
-        $record=new record();
+        $record=new record($array);
+
+
 
         return $record;
     }
